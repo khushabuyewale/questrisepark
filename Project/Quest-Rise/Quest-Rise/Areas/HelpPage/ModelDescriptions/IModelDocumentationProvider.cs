7a6f4e85c7e6e115ec555fb9196667c542c3efc7
Ashutosh <sonawaneashutosh3152000@gmail.com>
2024-02-19 16:52:59 +0530

    backend added to repo

---
+++ Project/Quest-Rise/Quest-Rise/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Quest_Rise.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
